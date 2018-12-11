0afd39b5f40770e206fde86bbe08ae2e4912b7a4
lonelyhentai <master@evernightfireworks.com>
2018-12-11 23:38:15 +0800

    docker-compose,server::Dockerfile

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,27 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const config = configService.get();
-        return {
-          type: config.DB_TYPE,
-          host: config.DB_HOSTNAME,
-          port: config.DB_PORT,
-          username: config.DB_USERNAME,
-          password: config.DB_PASSWORD,
-          database: config.DB_NAME,
-          entities: [__dirname +  "/**/*.entity{.ts,.js}"],
-          synchronize: true,
-        };
-      },
-    }),
-    CoreModule,
-    RoleModule,
-    AuthModule,
+    ConfigModule
   ],
 })
 export class AppModule {
